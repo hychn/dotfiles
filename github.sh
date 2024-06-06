914fd0a8e04937f9d61d1f39e8c427f7e5e5fd60
hychn <89435119+hychn@users.noreply.github.com>
2024-06-06 10:27:47 +0900

    Update github.sh

--- github.sh
+++ github.sh
@@ -9,3 +9,4 @@ cat ~/.ssh/id_ed25519.pub
 git clone ssh://git@github.com/hychnh/notes
 git config --global user.name "hychn"
 git config --global user.email "hychn@gmail.com"
+git clone ssh://git@github.com/hychn/notes
