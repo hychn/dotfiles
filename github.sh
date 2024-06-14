28086d76526378ea7847d011eaa5b4f6e8b3aac6
hychn <89435119+hychn@users.noreply.github.com>
2024-06-14 14:06:24 -0600

    Update github.sh

--- github.sh
+++ github.sh
@@ -6,7 +6,7 @@ ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub
 #add the output to the github ssh key
 
-git clone ssh://git@github.com/hychnh/notes
+
 git config --global user.name "hychn"
 git config --global user.email "hychn@gmail.com"
 git clone ssh://git@github.com/hychn/notes
